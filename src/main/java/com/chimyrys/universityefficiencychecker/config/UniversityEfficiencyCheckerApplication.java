347a46cf21ebb89dd7bb9156eddaa0ff2648a70d
Yuliia Chimyrys <yuch0421@netcracker.com>
2022-05-05 23:23:26 +0100

    Initial commit

---
+++ src/main/java/com/chimyrys/universityefficiencychecker/config/UniversityEfficiencyCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.chimyrys.universityefficiencychecker.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityEfficiencyCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityEfficiencyCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chimyrys/universityefficiencychecker/UniversityEfficiencyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimyrys.universityefficiencychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityEfficiencyCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
